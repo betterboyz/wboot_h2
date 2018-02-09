cebba1d2c13968196b847c0e02d51299273a657d
dsz <980770958@qq.com>
2018-02-09 09:21:10 +0800

    Initial commit

---
+++ src/main/java/com/dsz/WbootApplication.java
@@ -0,0 +1,12 @@
+package com.dsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbootApplication.class, args);
+	}
+}
