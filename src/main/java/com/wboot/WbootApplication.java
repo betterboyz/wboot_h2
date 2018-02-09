d6d975f23eb2dff004e11f97c32c8e6fbcf43811
dsz <980770958@qq.com>
2018-02-09 11:06:23 +0800

    版本1

--- src/main/java/com/wboot/WbootApplication.java
+++ src/main/java/com/wboot/WbootApplication.java
@@ -1,12 +1,21 @@
 package com.wboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wboot.**.dao")
 public class WbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WbootApplication.class, args);
+		System.out.println("ヾ(◍°∇°◍)ﾉﾞ    wboot启动成功      ヾ(◍°∇°◍)ﾉﾞ\n" +
+			" ______                    _   ______            \n" +
+			"|_   _ \\                  / |_|_   _ `.          \n" +
+			"  | |_) |   .--.    .--. `| |-' | | `. \\  .--.   \n" +
+			"  |  __'. / .'`\\ \\/ .'`\\ \\| |   | |  | |/ .'`\\ \\ \n" +
+			" _| |__) || \\__. || \\__. || |, _| |_.' /| \\__. | \n" +
+			"|_______/  '.__.'  '.__.' \\__/|______.'  '.__.'  ");
 	}
 }
---
+++ src/main/java/com/wboot/system/controller/WbootController.java
@@ -0,0 +1,13 @@
+package com.wboot.system.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author dsz
+ * @date 18/02/09
+ */
+@Controller
+public class WbootController {
+
+
+}
